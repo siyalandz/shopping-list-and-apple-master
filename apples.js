57c03e0d1156eec463421589971cb813c1486427
Siyanda <siyalandz@gmail.com>
2019-08-12 17:19:33 +0200

    my test

---
+++ apples.js
@@ -0,0 +1,67 @@
+
+
+function totalNumberSold(applesSoldList) {
+	var total = 0;
+	for(var i=0; i<applesSoldList.length;i++){
+	
+	}
+	return soldList;
+}
+
+function totalIncome(applesSoldList) {
+	var Allincome = 0;
+	for(var i=0;i<applesSoldList.length;i++){
+		
+	}
+	return Allincome;
+}
+
+function totalIncomeGreenApples(applesSoldList){
+	var allGreenQnty = 0;
+	for(var i=0; i<applesSoldList.length;i++){
+		 
+}
+return allGreenQnty;
+	
+}
+function totalProfit(applesSoldList, applePriceMap){
+	for (var i = 0; i<applesSoldList.length;i++){
+		applesSoldList.push(applePriceMap);
+	
+	return allProfit ;
+}
+}
+// -- extra functions...
+
+function totalPerColorSold(apples, color) {
+	for(var i = 0;i<apples.length;i++){
+		apples.push(color)
+	
+	return soldApples;
+}
+}
+function totalIncomePerColor(apples, color) {
+	var apples = [];
+	for (var i= 0; i<apples.length;i++){
+		apples.push(color)
+	
+	return colorIncome;
+}
+}
+function totalProfitPerColor(apples, color, costMap) {
+	var costMap ={}
+	for(var i= 0; i<costMap.length;i++){
+		apples.push(color)
+
+	
+	return colorTotal;
+}
+
+}
+function mostProfitableColor(apples, color, costMap) {
+	for(var i=0; i< costMap.length;i++){
+		apples.push(color)
+	
+	return colorProfit;
+}
+}
---
+++ shopping-list-dom.js
@@ -0,0 +1,14 @@
+var userNameElem = document.querySelector(".username");
+// var addElem = document.querySelector(".add");
+var mybtnElem = document.querySelector(".mybtn");
+var addElem = document.querySelector(".add");
+var addTextElem = document.querySelector(".text")
+var maliElem = document.querySelector(".mali")
+function addBtnClicked(){
+    alert("yebow");
+
+//   var yebo =   document.querySelector("input[name = 'myRadio']:checked")
+// // yebo = addTextElem.Value;
+
+}
+mybtnElem.addEventListener('click',addBtnClicked)
